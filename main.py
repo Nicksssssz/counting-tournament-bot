0d1f777251047aeb6028f18dd42bbbda94f92c64
Nicksssssz <nicolas.machado1704@gmail.com>
2026-01-28 17:32:17 -0300

    longest8

--- main.py
+++ main.py
@@ -26,8 +26,9 @@ bot = commands.Bot(command_prefix=None, intents=intents)
 # -------- CHANNELS --------
 TRACK_CHANNELS = {
     1060539711871004734, #commands
-    1315525836341907560,  #classic col
-    1315492435115114517  # contando col
+    1315525836341907560, #classic col
+    1315492435115114517, # contando col
+    1052340912216358993  # juiz
 }
 
 COMMANDS_CHANNEL_ID = 1060539711871004734
